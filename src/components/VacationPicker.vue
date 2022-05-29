db10694231a83765565ee5958e146ad01c774f9c
Yilmaz-B <302332077@student.rocmondriaan.nl>
2022-05-30 00:09:02 +0200

    worked on vacationpicker

---
+++ src/components/VacationPicker.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <h1>{{header}}</h1>
+        <p>Teller= {{counter}}</p>
+        <button v-on:click="counter++">+</button>
+        <button v-on:click="counter--">-</button>
+        <p>Teller 2= {{count}}</p>
+        <button @click="increment()" class="btn btn-success">+</button>
+        <button @click="decrement()" class="btn btn-danger">-</button>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="country in countryData.countries" v-bind:key="country.id">
+                {{country.id}} - {{country.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import countryData from '@/data/countryData'
+
+export default {
+    name: 'VacationPicker',
+    data(){
+        return {
+            countryData,
+            header: 'Vue Vacation Picker',
+            counter: 0,
+            count: 0,
+        }
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            this.count--;
+        }
+    }
+};
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/data/countryData.js
@@ -0,0 +1,30 @@
+const countryData = {
+    countries: [
+        {
+            id: 1,
+            name: 'USA',
+            capital: 'Washington',
+            cost: 1250,
+            details: 'United states are among the most visited country in the world.',
+            img: 'washington.jpg',
+        },
+        {
+            id: 2,
+            name: 'Netherlands',
+            capital: 'Amsterdam',
+            cost: 795,
+            details: 'The capital of the Netherlands, Amsterdam is over 1000 years old.',
+            img: 'amsterdam.jpg'
+        },
+        {
+            id: 3,
+            name: 'Australia',
+            capital: 'Sydney',
+            cost: 2235,
+            details: 'Australia is known for it\'s many exotic animals.',
+            img: 'sydney.jpg'
+        }
+    ]
+};
+
+export default countryData;
