8217872fc5c3776e61ce3e5c42566ab4e7e5e7a9
Yilmaz-B <302332077@student.rocmondriaan.nl>
2022-05-31 00:31:13 +0200

    made a onclick, disabled button and a selector

--- src/components/VacationPicker.vue
+++ src/components/VacationPicker.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="container">
         <h1>{{header}}</h1>
-        <p>Teller= {{counter}}</p>
+        <!-- <p>Teller= {{counter}}</p>
         <button v-on:click="counter++">+</button>
         <button v-on:click="counter--">-</button>
         <p>Teller 2= {{count}}</p>
         <button @click="increment()" class="btn btn-success">+</button>
-        <button @click="decrement()" class="btn btn-danger">-</button>
+        <button @click="decrement()" class="btn btn-danger">-</button> -->
         <ul class="list-group">
-            <li class="list-group-item" v-for="country in countryData.countries" v-bind:key="country.id">
+            <li class="list-group-item"  @click="cCountry(country)" v-for="country in countryData.countries" v-bind:key="country.id">
                 {{country.id}} - {{country.name}}
             </li>
+            <li v-for="name in name" v-bind:key="name">{{name}} + {{hoi}} = {{name + hoi}}</li>
+            <li><button :disabled="isDisabled">Disable test button</button></li>
+            <li><select name="list" id="list" v-for="numbers in number[0]" v-bind:key="numbers.index">
+                    <option value="1" v-bind="number.index">1</option>
+                    <option value="2" v-bind="number.index">2</option>
+                    <option value="3" v-bind="number.index">3</option>
+                </select></li>
         </ul>
     </div>
 </template>
@@ -26,6 +33,10 @@ export default {
             header: 'Vue Vacation Picker',
             counter: 0,
             count: 0,
+            name: 1,
+            hoi: 2,
+            isDisabled: true,
+            number: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         }
     },
     methods: {
@@ -34,7 +45,10 @@ export default {
         },
         decrement() {
             this.count--;
-        }
+        },
+        cCountry(country) {
+            alert(`You selected: ${country.name}`)
+        },
     }
 };
 
